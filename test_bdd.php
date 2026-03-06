806a27ee486d446732f0cfaa755392e1e90be97c
Titouan Bousquin <suzette.bousquin@gmail.com>
2026-03-06 14:06:36 +0100

    test bdd.php

--- test_bdd.php
+++ test_bdd.php
@@ -15,9 +15,9 @@ $password = $_ENV['DB_PASS'];
 // 4. On lance la connexion PDO habituelle
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    echo "Super ! La connexion sécurisée a réussi.";
+    $pdo->query("SELECT * FROM ETUDIANT"); // Juste pour tester la connexion
+    $pdo->fetchAll(); // On récupère les résultats pour s'assurer que la requête fonctionne
+    echo $pdo;
     
 } catch(PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
